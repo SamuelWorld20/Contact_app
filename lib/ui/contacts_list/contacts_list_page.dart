8ed24b76130bda2e8a4eedc6a17391820560d64e
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2023-10-19 03:17:11 +0000

    vertical list

--- lib/ui/contacts_list/contacts_list_page.dart
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -4,7 +4,36 @@ class ContactsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Contacts')),
+      appBar: AppBar(
+        title: const Text('Contacts'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Contact test',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              'Contact test',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              'Contact test',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              'Contact test',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              'Contact test',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
