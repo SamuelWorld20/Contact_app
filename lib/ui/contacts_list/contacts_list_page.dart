1bdcd6653abf136207161e75736a3f91c50725cf
Samuel_Oluwaseyi <ejalonibuoluwaseyi@gmail.com>
2023-10-25 05:16:35 +0000

    displayed contacts

--- lib/ui/contacts_list/contacts_list_page.dart
+++ lib/ui/contacts_list/contacts_list_page.dart
@@ -1,7 +1,19 @@
+import 'package:demo/data/contact.dart';
 import 'package:flutter/material.dart';
 import 'package:faker/faker.dart';
+import './/data/contact.dart';
 
 class ContactsListPage extends StatelessWidget {
+  // underscore makes it a private modifier
+  List<Contact> _contacts = List.generate(
+    50,
+    (index) => Contact(
+      name: Faker().person.firstName() + ' ' + Faker().person.lastName(),
+      email: Faker().internet.freeEmail(),
+      phoneNumber: Faker().randomGenerator.integer(1000000).toString(),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,13 +21,11 @@ class ContactsListPage extends StatelessWidget {
         title: const Text('Contacts'),
       ),
       body: ListView.builder(
-        itemCount: 30,
+        itemCount: _contacts.length,
         // Runs and build every single list item
-        itemBuilder: (context, index) => Center(
-          child: Text(
-            Faker().person.firstName() + ' ' + Faker().person.lastName(),
-            style: TextStyle(fontSize: 30),
-          ),
+        itemBuilder: (context, index) => ListTile(
+          title: Text(_contacts[index].name),
+          subtitle: Text(_contacts[index].email),
         ),
       ),
     );
